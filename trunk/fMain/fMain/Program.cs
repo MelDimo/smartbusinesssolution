7dbd0ecd901586b6937a2663dcfc024296b57342
meldimo <meldimo@df6af466-88ec-3871-97b8-d3ff7be44681>
2013-09-12 06:13:57 +0000

    firstupload

---
+++ trunk/fMain/fMain/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml;
+using com.sbs.dll;
+using System.Drawing;
+using com.sbs.dll.utilites;
+
+namespace com.sbs.gui.main
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            //Читаем файл конфигурации
+            Config conf = new Config();
+            if (!conf.loadConfig()) return;
+
+            //Авторизуем пользователя, 
+            fLogIn flogin = new fLogIn();
+            flogin.Text = GValues.prgNameFull;
+            if (flogin.ShowDialog() != DialogResult.OK) return; //авторизация прошла не успешно - выходим из приложения
+
+            Application.Run(new fMain());
+        }
+
+        //private static bool loadConfig()
+        //{
+        //    string msgError = "В ходе разбора файла конфигурации произошли следующие ошибки:";
+
+        //    XmlDocument doc = new XmlDocument();
+        //    XmlNode node_ref_unit;
+        //    XmlNode node_dbmode;
+        //    XmlNode node_maindb;
+
+        //    try
+        //    {
+        //        doc.Load(GValues.fileSettingsPath);
+        //        node_ref_unit = doc.GetElementsByTagName("ref_unit")[0];
+        //        node_dbmode = doc.GetElementsByTagName("dbmode")[0];
+        //        node_maindb = doc.GetElementsByTagName("maindb")[0];
+
+        //        if (!int.TryParse(node_ref_unit.InnerText, out GValues.unitID))
+        //            msgError += Environment.NewLine + "- Не удалось определить заведение;";
+            
+        //        GValues.DBMode = node_dbmode.InnerText;
+        //        if (GValues.DBMode.Length == 0)
+        //            msgError += Environment.NewLine + "- Не удалось определить режим взаисодействия с БД;";
+
+        //        GValues.mainDB = node_maindb.InnerText;
+        //        if (GValues.mainDB.Length == 0)
+        //            msgError += Environment.NewLine + "- Не удалось определить головную БД;";
+
+        //        if (!msgError.Equals("В ходе разбора файла конфигурации произошли следующие ошибки:"))
+        //        {
+        //            uMessage.Show(msgError + Environment.NewLine + Environment.NewLine + "Приложение будет закрыто.", SystemIcons.Error);
+        //            return false;
+        //        }
+        //    }
+        //    catch (Exception exc) { uMessage.Show("Неудалось прочесть файл конфигураций", exc, SystemIcons.Information); return false; }
+
+        //    return true;
+        //}
+    }
+}
---
+++ trunk/fMain/fMain/fMain.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace com.sbs.gui.main
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip_mainBottom = new System.Windows.Forms.StatusStrip();
+            this.SuspendLayout();
+            // 
+            // statusStrip_mainBottom
+            // 
+            this.statusStrip_mainBottom.Location = new System.Drawing.Point(0, 413);
+            this.statusStrip_mainBottom.Name = "statusStrip_mainBottom";
+            this.statusStrip_mainBottom.Size = new System.Drawing.Size(732, 22);
+            this.statusStrip_mainBottom.TabIndex = 1;
+            this.statusStrip_mainBottom.Text = "statusStrip1";
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 435);
+            this.Controls.Add(this.statusStrip_mainBottom);
+            this.IsMdiContainer = true;
+            this.Name = "fMain";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip_mainBottom;
+    }
+}
+
