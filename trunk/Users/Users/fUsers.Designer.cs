917ec755816914a416db38463d42494604f02277
meldimo <meldimo@df6af466-88ec-3871-97b8-d3ff7be44681>
2013-09-12 07:57:55 +0000

    firstUpload

---
+++ trunk/Users/Users/fUsers.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Users
+{
+    partial class fUsers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fUsers));
+            this.statusStrip_bottom = new System.Windows.Forms.StatusStrip();
+            this.tSSLabel_recCount = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStrip_top = new System.Windows.Forms.ToolStrip();
+            this.tSButton_add = new System.Windows.Forms.ToolStripButton();
+            this.tSButton_edit = new System.Windows.Forms.ToolStripButton();
+            this.tSButton_del = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tSButton_menu = new System.Windows.Forms.ToolStripButton();
+            this.tSButton_doc = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.tSComboBox_RecType = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.tSComboBox_organization = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripLabel3 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.statusStrip_bottom.SuspendLayout();
+            this.toolStrip_top.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip_bottom
+            // 
+            this.statusStrip_bottom.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tSSLabel_recCount});
+            this.statusStrip_bottom.Location = new System.Drawing.Point(0, 401);
+            this.statusStrip_bottom.Name = "statusStrip_bottom";
+            this.statusStrip_bottom.Size = new System.Drawing.Size(758, 22);
+            this.statusStrip_bottom.TabIndex = 6;
+            this.statusStrip_bottom.Text = "statusStrip_bottom";
+            // 
+            // tSSLabel_recCount
+            // 
+            this.tSSLabel_recCount.Name = "tSSLabel_recCount";
+            this.tSSLabel_recCount.Size = new System.Drawing.Size(105, 17);
+            this.tSSLabel_recCount.Text = "tSSLabel_recCount";
+            // 
+            // toolStrip_top
+            // 
+            this.toolStrip_top.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip_top.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tSButton_add,
+            this.tSButton_edit,
+            this.tSButton_del,
+            this.toolStripSeparator1,
+            this.tSButton_menu,
+            this.tSButton_doc});
+            this.toolStrip_top.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip_top.Name = "toolStrip_top";
+            this.toolStrip_top.Size = new System.Drawing.Size(758, 25);
+            this.toolStrip_top.TabIndex = 7;
+            this.toolStrip_top.Text = "toolStrip_top";
+            // 
+            // tSButton_add
+            // 
+            this.tSButton_add.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tSButton_add.Image = ((System.Drawing.Image)(resources.GetObject("tSButton_add.Image")));
+            this.tSButton_add.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tSButton_add.Name = "tSButton_add";
+            this.tSButton_add.Size = new System.Drawing.Size(23, 22);
+            this.tSButton_add.Text = "Добавить";
+            // 
+            // tSButton_edit
+            // 
+            this.tSButton_edit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tSButton_edit.Image = ((System.Drawing.Image)(resources.GetObject("tSButton_edit.Image")));
+            this.tSButton_edit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tSButton_edit.Name = "tSButton_edit";
+            this.tSButton_edit.Size = new System.Drawing.Size(23, 22);
+            this.tSButton_edit.Text = "Редактировать";
+            // 
+            // tSButton_del
+            // 
+            this.tSButton_del.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tSButton_del.Image = ((System.Drawing.Image)(resources.GetObject("tSButton_del.Image")));
+            this.tSButton_del.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tSButton_del.Name = "tSButton_del";
+            this.tSButton_del.Size = new System.Drawing.Size(23, 22);
+            this.tSButton_del.Text = "Удалить";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // tSButton_menu
+            // 
+            this.tSButton_menu.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tSButton_menu.Image = ((System.Drawing.Image)(resources.GetObject("tSButton_menu.Image")));
+            this.tSButton_menu.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tSButton_menu.Name = "tSButton_menu";
+            this.tSButton_menu.Size = new System.Drawing.Size(23, 22);
+            this.tSButton_menu.Text = "avaliable_mnu";
+            this.tSButton_menu.ToolTipText = "Доступные пункты меню";
+            // 
+            // tSButton_doc
+            // 
+            this.tSButton_doc.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tSButton_doc.Image = ((System.Drawing.Image)(resources.GetObject("tSButton_doc.Image")));
+            this.tSButton_doc.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tSButton_doc.Name = "tSButton_doc";
+            this.tSButton_doc.Size = new System.Drawing.Size(23, 22);
+            this.tSButton_doc.Text = "avaliable_doc";
+            this.tSButton_doc.ToolTipText = "Доступные документы";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.tSComboBox_RecType,
+            this.toolStripLabel2,
+            this.tSComboBox_organization,
+            this.toolStripLabel3,
+            this.toolStripComboBox1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 25);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(758, 25);
+            this.toolStrip1.TabIndex = 8;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(69, 22);
+            this.toolStripLabel1.Text = "Тип записи";
+            // 
+            // tSComboBox_RecType
+            // 
+            this.tSComboBox_RecType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.tSComboBox_RecType.Items.AddRange(new object[] {
+            "Пользователь",
+            "Группа"});
+            this.tSComboBox_RecType.Name = "tSComboBox_RecType";
+            this.tSComboBox_RecType.Size = new System.Drawing.Size(121, 25);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(79, 22);
+            this.toolStripLabel2.Text = "Организация";
+            // 
+            // tSComboBox_organization
+            // 
+            this.tSComboBox_organization.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.tSComboBox_organization.Name = "tSComboBox_organization";
+            this.tSComboBox_organization.Size = new System.Drawing.Size(121, 25);
+            // 
+            // toolStripLabel3
+            // 
+            this.toolStripLabel3.Name = "toolStripLabel3";
+            this.toolStripLabel3.Size = new System.Drawing.Size(64, 22);
+            this.toolStripLabel3.Text = "Заведение";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 25);
+            // 
+            // fUsers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 423);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.toolStrip_top);
+            this.Controls.Add(this.statusStrip_bottom);
+            this.Name = "fUsers";
+            this.Text = "Сотрудники";
+            this.statusStrip_bottom.ResumeLayout(false);
+            this.statusStrip_bottom.PerformLayout();
+            this.toolStrip_top.ResumeLayout(false);
+            this.toolStrip_top.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip_bottom;
+        private System.Windows.Forms.ToolStripStatusLabel tSSLabel_recCount;
+        private System.Windows.Forms.ToolStrip toolStrip_top;
+        private System.Windows.Forms.ToolStripButton tSButton_add;
+        private System.Windows.Forms.ToolStripButton tSButton_edit;
+        private System.Windows.Forms.ToolStripButton tSButton_del;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton tSButton_menu;
+        private System.Windows.Forms.ToolStripButton tSButton_doc;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripComboBox tSComboBox_RecType;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripComboBox tSComboBox_organization;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel3;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+
+    }
+}
+
